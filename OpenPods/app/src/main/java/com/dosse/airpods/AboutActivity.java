4a8ba85e33a804d173920346c0cc3712a35c9eef
Electric <itai.levin4@gmail.com>
2020-05-28 15:37:48 +0300

    Charge icon fix, hardcoded dimens & other misc fixes.

--- OpenPods/app/src/main/java/com/dosse/airpods/AboutActivity.java
+++ OpenPods/app/src/main/java/com/dosse/airpods/AboutActivity.java
@@ -10,25 +10,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
-    final String githubURL = "https://github.com/adolfintel/OpenPods";
-    final String websiteURL = "https://fdossena.com/?p=openPods/index.frag";
-    final String donateURL = "https://paypal.me/sineisochronic";
 
-    @SuppressWarnings("DanglingJavadoc")
+    public static final String websiteURL = "https://fdossena.com/?p=openPods/index.frag";
+    public static final String githubURL = "https://github.com/adolfintel/OpenPods";
+    public static final String donateURL = "https://paypal.me/sineisochronic";
+    public static final String fdroidURL = "https://f-droid.org/packages/com.dosse.airpods/";
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        /**
-         * LEGAL DANGER ZONE!
-         *
+        // --------------- LEGAL DANGER ZONE! --------------- //
+
+        /*
          * The following actions are a violation of the GNU GPLv3 License:
          * - Removing, hiding or altering the license or the code that displays it
          * - Removing or changing the link to the original source code or the code that displays it
          * - Removing or changing the donation link of the original project or the code that displays it
          *
-         * If you're forking the application, it MUST be made clear that your version is a fork, who the original author is, and all references to the original project and license must not be removed.
+         * If you're forking the application, it MUST be made clear that your version is a fork,
+         * who the original author is, and all references to the original project and license must not be removed.
          * You are free to add links to your project, donations, whatever to this activity.
          *
          * I am constantly monitoring all major Android app stores for violations. You have been warned.
@@ -36,14 +38,14 @@ public class AboutActivity extends AppCompatActivity {
 
         ((WebView)(findViewById(R.id.license))).loadUrl("file:///android_asset/license.html");
 
-        findViewById(R.id.github).setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(githubURL))));
         findViewById(R.id.website).setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(websiteURL))));
+        findViewById(R.id.github).setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(githubURL))));
         findViewById(R.id.donate).setOnClickListener(v -> {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(donateURL)));
             Toast.makeText(AboutActivity.this, "❤️", Toast.LENGTH_SHORT).show();
         });
 
-        //END OF LEGAL DANGER ZONE
+        // ------------ END OF LEGAL DANGER ZONE ------------ //
     }
 
 }
