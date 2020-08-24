9c86da711502a5e01034a2d096636469eabee576
Electric <itai.levin4@gmail.com>
2020-08-24 14:47:22 +0300

    Updated build tools

--- OpenPods/app/src/main/java/com/dosse/airpods/SettingsFragment.java
+++ OpenPods/app/src/main/java/com/dosse/airpods/SettingsFragment.java
@@ -2,7 +2,6 @@ package com.dosse.airpods;
 
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
