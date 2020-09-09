6718d0fb1347c2463df20690fe802aa65ae57288
idkwuu <idkwhatusernameuse@gmail.com>
2020-09-09 14:20:26 -0600

    Fix view visibility in PodsWidget

--- OpenPods/app/src/main/java/com/dosse/airpods/PodsWidget.java
+++ OpenPods/app/src/main/java/com/dosse/airpods/PodsWidget.java
@@ -22,6 +22,7 @@ public class PodsWidget extends AppWidgetProvider {
             views.setProgressBar(R.id.leftPodProgress, 100, leftStatus * 10, false);
             String podText_Left = (leftStatus == 10) ? "100%" : ((leftStatus < 10) ? ((leftStatus * 10 + 5) + "%") : "");
             views.setTextViewText(R.id.leftPodText, podText_Left);
+            views.setViewVisibility(R.id.left, View.VISIBLE);
         } else {
             views.setViewVisibility(R.id.left, View.GONE);
         }
@@ -30,6 +31,7 @@ public class PodsWidget extends AppWidgetProvider {
             views.setProgressBar(R.id.rightPodProgress, 100, rightStatus * 10, false);
             String podText_Right = (rightStatus == 10) ? "100%" : ((rightStatus < 10) ? ((rightStatus * 10 + 5) + "%") : "");
             views.setTextViewText(R.id.rightPodText, podText_Right);
+            views.setViewVisibility(R.id.right, View.VISIBLE);
         } else {
             views.setViewVisibility(R.id.right, View.GONE);
         }
@@ -38,15 +40,16 @@ public class PodsWidget extends AppWidgetProvider {
             views.setProgressBar(R.id.caseProgress, 100, caseStatus * 10, false);
             String podText_Case = (caseStatus == 10) ? "100%" : ((caseStatus < 10) ? ((caseStatus * 10 + 5) + "%") : "");
             views.setTextViewText(R.id.caseText, podText_Case);
+            views.setViewVisibility(R.id.casePods, View.VISIBLE);
         } else {
             views.setViewVisibility(R.id.casePods, View.GONE);
         }
 
-        /*if (leftStatus == 15 && rightStatus == 15 && caseStatus == 15) {
+        if (leftStatus == 15 && rightStatus == 15 && caseStatus == 15) {
             views.setViewVisibility(R.id.title, View.VISIBLE);
         } else {
-            views.setScrollPosition(R.id.title, View.GONE);
-        }*/
+            views.setViewVisibility(R.id.title, View.GONE);
+        }
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
